ef7c0c797acb4690d72f2441335b2f3507d09d1d
Om Pandey <om.pandey@crownstack.com>
2021-03-03 22:57:17 +0530

    feature: typeorm Integration

--- best-practices/nest-typeorm-graphQL/src/app.module.ts
+++ best-practices/nest-typeorm-graphQL/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: false,
+      logging: ['query', 'error'],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
