79296c44b34cbb85eb2d92ae372e5ceb1f402ce2
Om Pandey <om.pandey@crownstack.com>
2021-03-22 10:57:27 +0530

    refactor: configuration

--- best-practices/nest-typeorm-graphQL/src/app.module.ts
+++ best-practices/nest-typeorm-graphQL/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
@@ -12,6 +13,7 @@ import { typeOrmConfig } from './config/typeorm.config';
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
--- best-practices/nest-typeorm-graphQL/src/config/typeorm.config.ts
+++ best-practices/nest-typeorm-graphQL/src/config/typeorm.config.ts
@@ -1,15 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.TYPEORM_HOST || 'localhost',
+  host: process.env.TYPEORM_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.TYPEORM_USERNAME || 'postgres',
+  username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE || 'gq',
+  database: process.env.TYPEORM_DATABASE,
   entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
   synchronize: false,
   logging: ['query', 'error'],
